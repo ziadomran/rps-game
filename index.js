dbb996928f712e53259dd4806a6629893f54b26d
ziadomran <daiznarmo@hotmail.com>
2023-09-02 02:10:45 +0300

    Fix script
    Added Computer Score
    Added Player Score
    Added New functions

--- index.js
+++ index.js
@@ -3,24 +3,52 @@ let choices = [ 'rock' , 'paper', 'scissors']
 let computer = choices[Math.floor(Math.random()*choices.length)];
 return computer;
 }
+let Pscore =0;
+let Cscore =0;
 
-function round(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection){
 
-    if ( playerSelection === 'rock' && computerSelection === 'scissors' ){
-        alert("Player wins!")
+ 
+
+
+    if ( (playerSelection === 'rock' && computerSelection === 'scissors' ) || 
+        (playerSelection === 'scissors' && computerSelection === 'paper') ||
+        playerSelection === 'paper' && computerSelection === 'rock' )
+        {
+
+        console.log("Player wins!")
+        Pscore ++;
 
     }
-    else if (playerSelection === 'rock' && computerSelection === 'paper')
+    else if ( (playerSelection === 'rock' && computerSelection === 'paper') || 
+    (playerSelection === 'scissors' && computerSelection === 'rock') ||
+    playerSelection === 'paper' && computerSelection === 'scissors' ) 
     {
-        alert("Player loses!")
+        console.log("Player loses!")
+        Cscore++;
     } 
-
-
-
-
+    else if (playerSelection === computerSelection)
+    {
+        console.log("Tie!")
+    }
+    else console.log("You didn't select an appropriate hand")
 }
 
-const playerSelection = (prompt("Say your input: ")).toLowerCase();
-const computerSelection = getcomputerChoice()
 
-round(playerSelection, computerSelection)
+
+
+// playRound(playerSelection, computerSelection)
+
+
+function game(){
+
+
+    for (gamesPlayed = 0; gamesPlayed <5; gamesPlayed++){
+        const playerSelection = (prompt("Say your input: ")).toLowerCase();
+        const computerSelection = getcomputerChoice();
+        playRound(playerSelection, computerSelection)
+    }
+    alert(`Final Score is: Player ${Pscore} - ${Cscore} Computer` )
+}
+
+game()
